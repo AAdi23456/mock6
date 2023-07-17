b2e9cf1b2382ba7203067e1def6e10d5d660aeb8
AAdi23456 <118712556+AAdi23456@users.noreply.github.com>
2023-07-17 14:43:51 +0530

    done

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -3,16 +3,15 @@ const leaderboardmpodel = require("../models/leaderboard");
 const express=require("express")
 const router=express.Router()
 router.get('/show/:id', async (req, res) => {
-    try {
-        const {id}=req.params
-      const leaderboard = await leaderboardmpodel.find({quiz:id})
-      res.json(leaderboard);
-    } catch (error) {
-
-        console.log(error);
-      res.status(500).json({ error: 'Failed to fetch leaderboard entries' });
-    }
-  });
+  try {
+    const { id } = req.params;
+    const leaderboard = await leaderboardmpodel.find({ quiz: id }).sort({ score: -1 });
+    res.json(leaderboard);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Failed to fetch leaderboard entries' });
+  }
+});
   
  
   router.post('/new', async (req, res) => {
