4f19f81391d4ca86b418f3c8782a6fb898df109f
Aaditya Kumar Jha <118712556+AAdi23456@users.noreply.github.com>
2023-07-17 13:11:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const quizroutes = require("./routes/quizes");
 const users = require("./routes/users");
 const express = require('express');
+const cors=require("cors")
 const leaderboard=require("./routes/leaderboard")
 const app = express();
 app.use(express.json());
+app.use(cors())
 const connection = require("./database/mongodb");
 app.use("/leader",leaderboard)
 app.use('/reg', users); 
