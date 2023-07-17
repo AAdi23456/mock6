a5b122bd8afc3bc5d2cc20ff9276bfd974324852
AAdi23456 <118712556+AAdi23456@users.noreply.github.com>
2023-07-17 13:58:27 +0530

    done

--- routes/quizes.js
+++ routes/quizes.js
@@ -35,7 +35,7 @@ quizroutes.post('/quiz', async (req, res) => {
     try {
       const id=req.params.id
         const data=await quizmodel.findById(id)
-    return res.status(200).json(data.questions)
+    return res.status(200).json(data)
     } catch (error) {
       console.log(error);
         return res.status(500).json(error)
